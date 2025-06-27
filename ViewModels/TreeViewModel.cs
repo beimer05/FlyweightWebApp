7b9eb7661d876da0849f2195529d23c0d1b6b212
beimer05 <barroyor2022@gmail.com>
2025-06-27 14:45:45 -0500

    Create Tree view model

---
+++ ViewModels/TreeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FlyweightWebApp.ViewModels
+{
+    public class TreeViewModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string TreeName { get; set; }
+        public string Color { get; set; }
+        public string IconPath { get; set; }
+    }
+}
