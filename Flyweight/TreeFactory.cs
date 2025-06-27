cb70cd6c1a3d47a65cbb1f82b08ac9c16b861115
beimer05 <barroyor2022@gmail.com>
2025-06-27 14:29:24 -0500

    Create treefactory

---
+++ Flyweight/TreeFactory.cs
@@ -0,0 +1,23 @@
+﻿using FlyweightWebApp.Models;
+
+namespace FlyweightWebApp.Flyweight
+{
+    public class TreeFactory
+    {
+        private readonly Dictionary<string, TreeType> _treeType = new Dictionary<string, TreeType>();
+        public TreeType GetTreeType(string name, string color, string iconPath)
+        {
+            var key = $"{name}-{color}-{iconPath}";
+            if (!_treeType.ContainsKey(key))
+            {
+                _treeType[key] = new TreeType
+                {
+                    Name = name,
+                    Color = color,
+                    IconPath = iconPath
+                };
+            }
+            return _treeType[key];
+        }
+    }
+}
