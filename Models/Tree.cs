ca705853a5d00f2384236ff4be811aa73fbb698f
beimer05 <barroyor2022@gmail.com>
2025-06-27 14:26:25 -0500

    Create models

---
+++ Models/Tree.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace FlyweightWebApp.Models
+{
+    public class Tree
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public TreeType Type { get; set; }
+    }
+}
---
+++ Models/TreeType.cs
@@ -0,0 +1,10 @@
+﻿namespace FlyweightWebApp.Models
+{
+    public class TreeType
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public string IconPath { get; set; }
+    }
+}
