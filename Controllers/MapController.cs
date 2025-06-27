9bc498615b5a6bffecb2eac0321d6ad378015693
beimer05 <barroyor2022@gmail.com>
2025-06-27 14:50:19 -0500

    Create Mapcontroller

---
+++ Controllers/MapController.cs
@@ -0,0 +1,31 @@
+﻿using FlyweightWebApp.Services;
+using FlyweightWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyweightWebApp.Controllers
+{
+    public class MapController : Controller
+    {
+
+        private readonly ITreeService _treeService;
+
+        public MapController(ITreeService treeService)
+        {
+            _treeService = treeService;
+        }
+        public IActionResult Index()
+        {
+            var treeEntities = _treeService.GetAllTrees();
+            var viewModel = treeEntities.Select(tree => new TreeViewModel
+            {
+                Latitude= tree.Latitude,
+                Longitude= tree.Longitude,
+                TreeName= tree.Type.Color,
+                Color= tree.Type.Color,
+                IconPath = tree.Type.IconPath,
+               
+            }).ToList();
+            return View(viewModel);
+        }
+    }
+}
