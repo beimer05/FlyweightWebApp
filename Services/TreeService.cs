58c7b5051e356311ae95ee79ccf83c6216edde84
beimer05 <barroyor2022@gmail.com>
2025-06-29 15:22:59 -0500

    Add Method and Implement to TreeService class

--- Services/TreeService.cs
+++ Services/TreeService.cs
@@ -7,22 +7,43 @@ namespace FlyweightWebApp.Services
     {
 
         private readonly TreeFactory _treeFactory;
+        private readonly List<Tree> _trees = new List<Tree>();
+
+        private readonly Dictionary<string,(string color,string iconPath)> _speciesCatalog = new Dictionary<string, (string color, string iconPath)>
+        {
+            { "Oak", ("Green", "~/image/Oak.png") },
+            { "Pine", ("DarkGreen", "~/image/Pine.png") },
+            { "Maple", ("Brown", "~/image/Maple.png") }
+        };
 
         public TreeService()
         {
             _treeFactory = new TreeFactory();
-        }
-        public IEnumerable<Tree> GetAllTrees()
-        {
-            return new List<Tree>
+            foreach(var species in _speciesCatalog)
             {
-                new Tree { Latitude = 34.0522, Longitude = -118.2437, Type = _treeFactory.GetTreeType("Oak","Green","~/image/Oak.png") },
-                new Tree { Latitude = 34.0522, Longitude = -118.2437, Type = _treeFactory.GetTreeType("Pine","DarkGreen","~/image/Pine.png") },
-                new Tree { Latitude = 34.0522, Longitude = -118.2437, Type = _treeFactory.GetTreeType("Maple","Brown","~/image/Maple.png") },
-                new Tree { Latitude = 34.0522, Longitude = -118.2437, Type = _treeFactory.GetTreeType("Oak","Green","~/image/Oak.png") },
-                new Tree { Latitude = 34.0522, Longitude = -118.2437, Type = _treeFactory.GetTreeType("Pine","DarkGreen","~/image/Pine.png") }
-            };
-           
+                _treeFactory.GetTreeType(species.Key, species.Value.color, species.Value.iconPath);
+            }
         }
+
+        public void AddTree(string species, double latitude, double longitude)
+        {
+           if(!_speciesCatalog.ContainsKey(species)) throw new ArgumentException($"Species {species} is not recognized.");
+            var (color, iconPath) = _speciesCatalog[species];
+            var type = _treeFactory.GetTreeType(species, color, iconPath);
+            _trees.Add(new Tree
+            {
+                Latitude = latitude,
+                Longitude = longitude,
+                Type = type
+            });
+
+        }
+
+    
+
+        public List<string> GetAvailableSpecies() => _speciesCatalog.Keys.ToList();
+
+        public List<Tree> GetAllTrees() => _trees;  
+         
     }
 }
