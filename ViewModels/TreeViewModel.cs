b40ba0dd6a90fd28187b7a34657a4009e2d018e6
beimer05 <barroyor2022@gmail.com>
2025-06-29 16:11:13 -0500

    Add nullables to member of treeviewModel

--- ViewModels/TreeViewModel.cs
+++ ViewModels/TreeViewModel.cs
@@ -5,14 +5,14 @@ namespace FlyweightWebApp.ViewModels
     public class TreeViewModel
     {
         [Required(ErrorMessage = "Species is required")]
-        public string Species { get; set; }
+        public string? Species { get; set; }
 
         [Required(ErrorMessage = "Latitude is required")]
         public double? Latitude { get; set; }
 
         [Required(ErrorMessage = "Longitude is required")]
         public double? Longitude { get; set; }
-        public List<string> AvailableSpecies { get; set; }
+        public List<string>? AvailableSpecies { get; set; }
 
     }
 }
